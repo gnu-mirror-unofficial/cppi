87d985c1fc11f5016a881a2f05ae369b3dcf591e
Jim Meyering <jim@meyering.net>
1994-11-22 20:20:30 +0000

    .

---
+++ src/cpp-indent.pl
@@ -0,0 +1,59 @@
+#!/p/bin/perl5.000
+
+$depth = 0;
+$indent_incr = '  ';
+$do_comments = 0;
+
+while (<>)
+  {
+    if (/^#/)
+      {
+	if (/^#\s*(if(n?def)?)\b(.*)/)
+	  {
+	    $indent = $indent_incr x $depth;
+	    print "#$indent$1$3\n";
+	    ++$depth;
+	  }
+	elsif (/^#\s*else\b(.*)/)
+	  {
+	    $indent = $indent_incr x ($depth - 1);
+	    print "#${indent}else$1\n";
+	  }
+	elsif (/^#\s*endif\b(.*)/)
+	  {
+	    --$depth;
+	    $indent = $indent_incr x $depth;
+	    print "#${indent}endif$1\n";
+	  }
+	elsif (/^#\s*(.*)/)
+	  {
+	    $indent = $indent_incr x $depth;
+	    print '#', $indent, $1, "\n";
+	  }
+	else
+	  {
+	    die;
+	  }
+      }
+    else
+      {
+        if ($do_comments)
+	  {
+	    if (/^(\/\*.*)/)
+	      {
+		$indent = $indent_incr x $depth;
+		print " ${indent}$_";
+	      }
+	    else
+	      {
+		print;
+	      }
+	  }
+	else
+	  {
+	    print;
+	  }
+      }
+  }
+
+exit (0);
