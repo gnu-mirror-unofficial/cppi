11935fe45de439d79868704706f1c341b7a42dff
Jim Meyering <jim@meyering.net>
1997-04-29 12:39:02 +0000

    *** empty log message ***

--- src/cpp-indent.pl
+++ src/cpp-indent.pl
@@ -3,84 +3,110 @@
 # written by Jim Meyering
 use strict;
 
-# Bugs:
-# May get confused by comments or string literals containing
-# what would otherwise be valid cpp directives.
-
-# TODO: allow these to be overridden by command-line options
-my $indent_incr = ' ';
-
-my @opener_stack;
-my $depth = 0;
-
 unshift (@ARGV, '-') if @ARGV == 0;
 die "usage: $0 [FILE]\n" if @ARGV > 1;
 
 my $file = shift @ARGV;
-
-open (FILE, $file) || die "$0: couldn't open $file: $!\n";
-
-my $exit_status = 0;
-my $line;
-while (defined ($line = <FILE>))
-  {
-    if ($line =~ s/^\s*\#\s*//)
-      {
-	my $keyword;
-	my $indent;
-	if ($line =~ /^if(n?def)?\b/)
-	  {
-	    # Maintain stack of (line number, keyword) pairs to better
-	    # report any `unterminated #if...' errors.
-	    push @opener_stack, {LINE_NUMBER => $., KEYWORD => $&};
-	    $keyword = $&;
-	    $indent = $indent_incr x $depth;
-	    ++$depth;
-	  }
-	elsif ($line =~ /^(else|elif)\b/)
-	  {
-	    if ($depth < 1)
-	      {
-		warn "$0: $file: line $.: found #$& without matching #if\n";
-		$depth = 1;
-		$exit_status = 1;
-	      }
-	    $keyword = $&;
-	    $indent = $indent_incr x ($depth - 1);
-	  }
-	elsif ($line =~ /^endif\b/)
-	  {
-	    if ($depth < 1)
-	      {
-		warn "$0: $file: line $.: found #$& without matching #if\n";
-		$depth = 1;
-		$exit_status = 1;
-	      }
-	    $keyword = $&;
-	    --$depth;
-	    $indent = $indent_incr x $depth;
-	    pop @opener_stack;
-	  }
-	else
-	  {
-	    $keyword = '';
-	    $indent = $indent_incr x $depth;
-	  }
-
-	$line = "#$indent$keyword$'";
-      }
-    print $line;
-  }
-close FILE;
-
-if ($depth != 0)
-  {
-    my $x;
-    foreach $x (@opener_stack)
-      {
-	warn "$0: $file: line $x->{LINE_NUMBER}: unterminated #$x->{KEYWORD}\n"
-      }
-    $exit_status = 1;
-  }
+my $exit_status = cpp_indent ($file, 0);
 
 exit ($exit_status);
+
+# ===============================
+
+# Return 2 for syntax problems.
+# Return 1 for invalid indentation of CPP #-directives (only if $checking).
+# if checking
+#   return 0 if syntax and indentation are valid
+# else
+#   if (syntax is valid)
+#     {
+#       print properly indented code to stdout
+#       return 0
+#     }
+sub cpp_indent ($$)
+{
+  my ($file, $checking) = @_;
+
+  # Bugs:
+  # May get confused by comments or string literals containing
+  # what would otherwise be valid cpp directives.
+
+  # TODO: allow this to be overridden by a command-line option.
+  my $indent_incr = ' ';
+
+  my @opener_stack;
+  my $depth = 0;
+
+  open (FILE, $file) || die "$0: couldn't open $file: $!\n";
+
+  my $fail = 0;
+  my $line;
+  while (defined ($line = <FILE>))
+    {
+      if ($line =~ s/^\s*\#\s*//)
+	{
+	  my $keyword;
+	  my $indent;
+	  if ($line =~ /^if(n?def)?\b/)
+	    {
+	      # Maintain stack of (line number, keyword) pairs to better
+	      # report any `unterminated #if...' errors.
+	      push @opener_stack, {LINE_NUMBER => $., KEYWORD => $&};
+	      $keyword = $&;
+	      $indent = $indent_incr x $depth;
+	      ++$depth;
+	    }
+	  elsif ($line =~ /^(else|elif)\b/)
+	    {
+	      if ($depth < 1)
+		{
+		  warn "$0: $file: line $.: found #$& without matching #if\n";
+		  $depth = 1;
+		  $fail = 2;
+		}
+	      $keyword = $&;
+	      $indent = $indent_incr x ($depth - 1);
+	    }
+	  elsif ($line =~ /^endif\b/)
+	    {
+	      if ($depth < 1)
+		{
+		  warn "$0: $file: line $.: found #$& without matching #if\n";
+		  $depth = 1;
+		  $fail = 2;
+		}
+	      $keyword = $&;
+	      --$depth;
+	      $indent = $indent_incr x $depth;
+	      pop @opener_stack;
+	    }
+	  else
+	    {
+	      $keyword = '';
+	      $indent = $indent_incr x $depth;
+	    }
+
+	  if ($checking && $line ne "#$indent$keyword$'")
+	    {
+	      close FILE;
+	      return 1;
+	    }
+	  $line = "#$indent$keyword$'";
+	}
+      print $line if !$checking;
+    }
+  close FILE;
+
+  if ($depth != 0)
+    {
+      my $x;
+      foreach $x (@opener_stack)
+	{
+	  warn "$0: $file: line $x->{LINE_NUMBER}: "
+	    . "unterminated #$x->{KEYWORD}\n";
+	}
+      $fail = 2;
+    }
+
+  return $fail;
+}
