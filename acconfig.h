22e4959a565811705f51cb539c45c4fc4df250db
Jim Meyering <jim@meyering.net>
1998-03-15 20:46:28 +0000

    .

---
+++ acconfig.h
@@ -0,0 +1,10 @@
+/* Definitions for cppi, processed by autoheader. */
+
+/* The concatenation of the strings "GNU ", and PACKAGE.  */
+#undef GNU_PACKAGE
+
+/* Define to the name of the distribution.  */
+#undef PACKAGE
+
+/* Define to the version of the distribution.  */
+#undef VERSION
